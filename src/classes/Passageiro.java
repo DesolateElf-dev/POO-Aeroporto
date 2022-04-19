843be7aa6011c371f90ecb0ade5a24400c44f5b7
emaci <emaci@DESKTOP-EDU>
2022-04-18 22:58:42 -0300

    get&set para passageiros criado

--- src/classes/Passageiro.java
+++ src/classes/Passageiro.java
@@ -4,6 +4,29 @@ public class Passageiro extends Pessoa {
 	
 	private int codPassagem;
 	private double pesoBagagem;
-	private 		
+	private String destino;
+	
+	
+	
+	
+	
+	public int getCodPassagem() {
+		return codPassagem;
+	}
+	public void setCodPassagem(int codPassagem) {
+		this.codPassagem = codPassagem;
+	}
+	public double getPesoBagagem() {
+		return pesoBagagem;
+	}
+	public void setPesoBagagem(double pesoBagagem) {
+		this.pesoBagagem = pesoBagagem;
+	}
+	public String getDestino() {
+		return destino;
+	}
+	public void setDestino(String destino) {
+		this.destino = destino;
+	}
 
 }
